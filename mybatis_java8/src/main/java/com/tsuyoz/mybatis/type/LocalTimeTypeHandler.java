7b9c97eeecc945101758d1c8d1b07a85fa85f585
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2015-09-26 00:24:16 +0900

    change

--- mybatis_java8/src/main/java/com/tsuyoz/mybatis/type/LocalTimeTypeHandler.java
+++ mybatis_java8/src/main/java/com/tsuyoz/mybatis/type/LocalTimeTypeHandler.java
@@ -9,26 +9,26 @@ import java.time.LocalTime;
 public class LocalTimeTypeHandler extends BaseTypeHandler<LocalTime> {
 
 	@Override
-	public void setNonNullParameter(PreparedStatement ps, int i, LocalTime time, JdbcType jdbcType) throws SQLException {
+    public void setNonNullParameter(PreparedStatement ps, int i, LocalTime time, JdbcType jdbcType) throws SQLException {
         ps.setTime(i, Time.valueOf(time));
-	}
+    }
 
-	@Override
-	public LocalTime getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    @Override
+    public LocalTime getNullableResult(ResultSet rs, String columnName) throws SQLException {
         return toLocalTime(rs.getTime(columnName));
-	}
+    }
 
-	@Override
-	public LocalTime getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    @Override
+    public LocalTime getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
         return toLocalTime(rs.getTime(columnIndex));
-	}
+    }
 
-	@Override
-	public LocalTime getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    @Override
+    public LocalTime getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         return toLocalTime(cs.getTime(columnIndex));
-	}
-	
-	private LocalTime toLocalTime(Time time) {
+    }
+
+    private LocalTime toLocalTime(Time time) {
 		return time == null ? null : time.toLocalTime();
 	}
 }
